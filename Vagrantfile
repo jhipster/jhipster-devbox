1ca21e81f3d673566fe713d70dcf39584569915b
Pierre Besson <pbesson@ippon.fr>
2015-11-13 21:54:21 +0100

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,15 @@
+Vagrant.configure("2") do |config|
+    config.vm.box = "ubuntu/wily64"
+    config.vm.provision :shell, :path => "scripts/setup.sh"
+    config.vm.network :forwarded_port, host: 8080, guest: 8080
+    config.vm.network :forwarded_port, host: 3000, guest: 3000
+    config.vm.synced_folder "~/jhipster", "/home/vagrant/jhipster"
+
+    config.vm.provider :virtualbox do |vb|
+        vb.gui = true
+        # Use VBoxManage to customize the VM. For example to change memory:
+        vb.customize ["modifyvm", :id, "--memory", "4096"]
+        vb.customize ["modifyvm", :id, "--vram", 64]
+        vb.customize ["modifyvm", :id, "--accelerate3d", "on"]
+    end
+end
---
+++ scripts/setup.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+# update the system
+sudo apt-get update
+
+################################################################################
+# This is a port of the JHipster Dockerfile,
+# see https://github.com/jhipster/jhipster-docker/
+################################################################################
+
+export JHIPSTER_VERSION='2.23.1'
+
+export JAVA_VERSION='8'
+export JAVA_HOME='/usr/lib/jvm/java-8-oracle'
+
+export MAVEN_VERSION='3.3.3'
+export MAVEN_HOME='/usr/share/maven'
+export PATH=$PATH:$MAVEN_HOME/bin
+
+export LANGUAGE='en_US.UTF-8'
+export LANG='en_US.UTF-8'
+export LC_ALL='en_US.UTF-8'
+sudo locale-gen en_US.UTF-8
+sudo dpkg-reconfigure locales
+
+# install utilities
+sudo apt-get -y install vim git sudo zip bzip2 fontconfig curl
+
+# install Java 8
+sudo echo 'deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' >> /etc/apt/sources.list
+sudo echo 'deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' >> /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C2518248EEA14886
+
+sudo apt-get update
+
+sudo echo oracle-java-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get install -y --force-yes oracle-java${JAVA_VERSION}-installer
+sudo  update-java-alternatives -s java-8-oracle
+
+# install node.js
+sudo curl -sL https://deb.nodesource.com/setup_4.x | sudo bash -
+sudo apt-get install -y nodejs unzip python g++ build-essential
+
+# install yeoman grunt bower grunt gulp
+sudo npm install -g yo bower grunt-cli gulp
+
+# install JHipster
+sudo npm install -g generator-jhipster@${JHIPSTER_VERSION}
+
+################################################################################
+# Install the graphical environment
+################################################################################
+
+# force encoding
+sudo echo 'LANG=en_US.UTF-8' >> /etc/environment
+sudo echo 'LANGUAGE=en_US.UTF-8' >> /etc/environment
+sudo echo 'LC_ALL=en_US.UTF-8' >> /etc/environment
+sudo echo 'LC_CTYPE=en_US.UTF-8' >> /etc/environment
+
+# install languages
+sudo apt-get install language-pack-fr
+
+# run GUI as non-privileged user
+sudo echo 'allowed_users=anybody' > /etc/X11/Xwrapper.config
+
+# install Ubuntu desktop and VirtualBox guest tools
+sudo apt-get install -y ubuntu-desktop virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+
+################################################################################
+# Install the development tools
+################################################################################
+
+# install Eclipse
+sudo mkdir /home/vagrant/applications
+cd /home/vagrant/applications && sudo wget  http://dist.springsource.com/release/STS/3.7.1.RELEASE/dist/e4.5/spring-tool-suite-3.7.1.RELEASE-e4.5.1-linux-gtk-x86_64.tar.gz
+cd /home/vagrant/applications && sudo tar -zxvf spring-tool-suite-3.7.1.RELEASE-e4.5.1-linux-gtk-x86_64.tar.gz
+
+# install Chromium Browser
+sudo apt-get install -y chromium-browser
+
+# install MySQL
+#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+#sudo apt-get install -y mysql-server mysql-workbench
