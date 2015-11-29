a65a12880ef370fc10d48e8cb757d016213bdc32
Julien Dubois <julien.dubois@gmail.com>
2015-11-29 18:30:47 +0100

    Remove box size reducing code
    See #15

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -126,6 +126,4 @@ sudo chown -R vagrant:vagrant /home/vagrant
 
 # clean the box
 sudo apt-get clean
-sudo dd if=/dev/zero of=/EMPTY bs=1M
-sudo rm -f /EMPTY
 cat /dev/null > ~/.bash_history && history -c
