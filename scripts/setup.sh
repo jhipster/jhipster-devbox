92502319084b1dae45cc1badd1adb9224ed60472
Alberto Rocha <betogrun@gmail.com>
2016-04-24 21:24:44 -0300

    Add mongoDB

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -94,6 +94,14 @@ apt-get install -y chromium-browser
 # install MySQL Workbench
 apt-get install -y mysql-workbench
 
+# install MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+sudo apt-get update
+
+sudo apt-get install -y mongodb-org
+
 # install PgAdmin
 apt-get install -y pgadmin3
 
