6246ee6ff16cbbe5e121bb1bdbae90f2fbf94f61
Julien Dubois <julien.dubois@gmail.com>
2016-06-13 14:55:30 +0200

    Release 3.4.1

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -40,7 +40,7 @@ npm install -g npm
 npm install -g yo bower gulp
 
 # install JHipster
-npm install -g generator-jhipster@3.4.0
+npm install -g generator-jhipster@3.4.1
 
 # install JHipster UML
 npm install -g jhipster-uml@1.6.5
