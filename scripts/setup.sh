69218f5d076dce11a745cbb4d28742d9f046e676
Pierre Besson <pbesson@ippon.fr>
2016-08-18 16:10:59 +0200

    Release v3.6.1

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -40,7 +40,7 @@ npm install -g npm
 npm install -g yo bower gulp
 
 # install JHipster
-npm install -g generator-jhipster@3.6.0
+npm install -g generator-jhipster@3.6.1
 
 # install JHipster UML
 npm install -g jhipster-uml@1.6.5
