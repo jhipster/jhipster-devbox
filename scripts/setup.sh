7fe2585ee78bb917a7fb2f38099e1eee1a41df38
cbornet <cbornet@hotmail.com>
2016-09-21 11:16:35 +0200

    set zsh as default shell for user vagrant (used by guake)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -119,7 +119,7 @@ apt-get install -y zsh
 # install oh-my-zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
 cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
-chsh -s /bin/zsh
+chsh -s /bin/zsh vagrant
 echo 'SHELL=/bin/zsh' >> /etc/environment
 
 # install jhipster-oh-my-zsh-plugin
