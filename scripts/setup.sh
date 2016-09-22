b9516adf79c7ca951090ce56bafa16f07af9d1a0
Julien Dubois <julien.dubois@gmail.com>
2016-09-22 23:28:34 +0200

    Release v3.8.0

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -40,7 +40,7 @@ npm install -g npm
 npm install -g yo bower gulp
 
 # install JHipster
-npm install -g generator-jhipster@3.7.1
+npm install -g generator-jhipster@3.8.0
 
 # install JHipster UML
 npm install -g jhipster-uml@1.6.5
