50db1b77832a16e81b5e28e22c1990ffd69fe7e3
Julien Dubois <julien.dubois@gmail.com>
2018-06-19 14:23:57 +0200

    Release v4.14.5

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -35,7 +35,7 @@ su -c "yarn config set prefix /home/vagrant/.yarn-global" vagrant
 su -c "yarn global add yo" vagrant
 
 # install JHipster
-su -c "yarn global add generator-jhipster@4.14.4" vagrant
+su -c "yarn global add generator-jhipster@4.14.5" vagrant
 
 # install JHipster UML
 su -c "yarn global add jhipster-uml@2.0.3" vagrant
