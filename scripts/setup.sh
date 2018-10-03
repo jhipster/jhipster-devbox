9cd3d717a53c0c91e65fac73e2282858981cfeee
Julien Dubois <julien.dubois@gmail.com>
2018-10-03 16:23:35 +0200

    Release v5.4.0

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -29,7 +29,7 @@ su -c "yarn config set prefix /home/vagrant/.yarn-global" vagrant
 npm install -g yo
 
 # install JHipster
-npm install -g generator-jhipster@5.3.4
+npm install -g generator-jhipster@5.4.0
 
 # install JHipster UML
 npm install -g jhipster-uml@2.0.3
