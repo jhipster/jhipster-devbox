a0ec1c7566ecbf36254be1031e33ba7d6b71d906
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-11-19 16:59:05 +0100

    Release v7.4.0

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -32,7 +32,7 @@ su -c "yarn config set prefix /home/vagrant/.yarn-global" vagrant
 npm install -g yo
 
 # install JHipster
-npm install -g generator-jhipster@7.3.1
+npm install -g generator-jhipster@7.4.0
 
 # install JHipster UML
 npm install -g jhipster-uml@2.0.3
